88a62e9a49c96d5200894741298478d81ed49309
Schelde Jonas <scheldejonas@gmail.com>
2016-10-14 22:10:08 +0200

    Merge branch 'master' into misk
    # Conflicts:
    #	README.md

---
+++ src/domain/DomainFacade.java
@@ -0,0 +1,27 @@
+package domain;
+
+import dbaccess.DBFacade;
+import java.util.List;
+
+/**
+ * The purpose of DomainFacade is to provide a way for the presentation layer to
+ * access the domain model
+ *
+ * @author kasper
+ */
+public class DomainFacade {
+
+    public static List<Building> getBuildings() {
+        //return Generator.randomBuildings(100);
+        return DBFacade.getBuildings();
+    }
+
+    public static Building getBuilding( int id ) {
+        return DBFacade.getBuildings( id );
+    }
+
+    public static void updateBuilding( Building b ) {
+        DBFacade.updateBuilding( b );
+    }
+}
+ 
